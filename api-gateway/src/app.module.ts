c6b58b144141bcfedc696cf7826da32893c0019c
ViniciusDanielHub <viniciushubgit@hotmail.com>
2026-03-19 00:31:48 -0300

    Correcting the paths in the app.module of the api-gateway.

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -1,11 +1,32 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
+import { gatewayConfig } from './config/app.config';
 import { AuthModule } from './modules/auth/auth.module';
 import { CategoryModule } from './modules/category/category.module';
+import { ProductModule } from './modules/product/product.module';
+import { HealthModule } from './modules/health/health.module';
+import { UserModule } from './modules/user/user.module'; 
+import { AdminModule } from './modules/admin/admin.module'; 
+import { FileModule } from './modules/file/file.module'; 
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [gatewayConfig],
+    }),
+    HttpModule.register({
+      timeout: 10000,
+      maxRedirects: 5,
+    }),
     AuthModule,
     CategoryModule,
+    ProductModule,
+    HealthModule,
+    UserModule,
+    AdminModule,
+    FileModule,
   ],
 })
-export class AppModule {}
+export class AppModule { }
